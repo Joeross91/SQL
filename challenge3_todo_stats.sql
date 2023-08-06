177aefc28d024024cd2f133ecda24e6df66a696c
Joeross91 <136847585+Joeross91@users.noreply.github.com>
2023-08-05 22:28:30 -0500

    challenge3_todo_stats.sql
    completed coursework for intro to sql khan academy

--- challenge3_todo_stats.sql
+++ challenge3_todo_stats.sql
@@ -2,6 +2,7 @@
 Insert another item to your todo list with the estimated minutes it will take.
 Select the SUM of minutes it will take to do all of the items on your TODO list.*/
 
+
 CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
 INSERT INTO todo_list VALUES (2, "vacuuming", 20);
