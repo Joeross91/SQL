b9c5b3b3084da334b785ec1038f49d4aba8f3814
Joeross91 <136847585+Joeross91@users.noreply.github.com>
2023-08-05 22:25:56 -0500

    challenge1_book_list.sql

--- challenge1_book_list.sql
+++ challenge1_book_list.sql
@@ -1,6 +1,4 @@
 /*Challenge: Book list database
-
-Step 1
 Create a table to store a list of books. It should have columns for id, name, and rating. 
 Add three of your favorite books into the table.
 Answer */
